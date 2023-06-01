8660f1aa269e4e83e7fc472d10b5b24195f22f8e
CoochiecooBerrauslu <113683902+CoochiecooBerrauslu@users.noreply.github.com>
2023-06-01 21:17:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n1 = 0, n2 = 1,givenN;
+        // getting a number from the user
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many times would you like to run the Fibonacci Sequence : ");
       givenN = scanner.nextInt();
-
+        // to avoid getting negative number from the user
         while (givenN < 0) {
             System.out.println("Please be sure the number is bigger than 0 !" + "\n" +
                     "Enter the new number :");
@@ -15,8 +16,9 @@ public class Main {
         }
 
         System.out.print("Fibonacci series till " + givenN + " " + "times"+ ":"  );
-
+        // getting fibonacci series with a loop
         for(int i = 0; i <= givenN; i++) {
+            //putting comma while sequencing the number and not letting it put comma to the end.
             if(n1 == 0){
                 System.out.print(n1);
             }else{
